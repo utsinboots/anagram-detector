f655e3dc355d3afac8887d8abc25b5105a6f5d3c
github username <github email address>
2021-07-18 16:16:26 +0545

    commit tree 65d2026eb349cfcc70d4dff7ef2700e3e25107e8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    
+   def match(anagram_array)
+       array_match = []
+       sorted_word = @word.split("").sort.join
+       anagram_array.each do |array_word|
+           sorted_anagram = array_word.split("").sort.join
+           if sorted_anagram == sorted_word
+               array_match << array_word
+           end
+       end
+       array_match
+    end
+end
+
+#Another more simpler way to get the matching anagram word is by using .select which returns 
+#the words in array when the condition is true
+#
+#def match(anagram_array)
+#    anagram_array.select do |w|
+#        w.split("").sort == word.split("").sort
+#    end
+#end
